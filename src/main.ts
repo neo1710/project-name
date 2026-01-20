c8b938630895ad05b9e754fb49a6bf4688b58709
neo1710 <neeraj3dworld@gmail.com>
2026-01-20 21:36:41 +0530

    cors fixed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add a simple GET route at "/"
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.send('hello I am setup for this');
+  });
+
+    // Enable CORS for all origins
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
